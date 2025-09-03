3747da7f6fc81bf61d067a975d46345e92da04f6
Natan Gomes <natantatan2000@gmail.com>
2025-09-03 17:07:05 -0300

    edição painel sidebar

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,187 +1,175 @@
-"use client"
-import * as React from "react"
+'use client';
+import * as React from 'react';
 import {
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  LifeBuoy,
-  Map,
-  PieChart,
-  Send,
-  Settings2,
-  SquareTerminal,
-} from "lucide-react"
-import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavSecondary } from "@/components/nav-secondary"
-import { NavUser } from "@/components/nav-user"
+    Command,
+    Frame,
+    LayoutDashboard,
+    LifeBuoy,
+    Map,
+    PieChart,
+    Send,
+    Settings2,
+    UserPlus,
+    UserRoundSearchIcon,
+} from 'lucide-react';
+import { NavMain } from '@/components/nav-main';
+import { NavProjects } from '@/components/nav-projects';
+import { NavSecondary } from '@/components/nav-secondary';
+import { NavUser } from '@/components/nav-user';
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import { useAuth } from "@/features/auth/hooks/useAuth"
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { useAuth } from '@/features/auth/hooks/useAuth';
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Playground",
-      url: "#",
-      icon: SquareTerminal,
-      isActive: true,
-      items: [
-        {
-          title: "History",
-          url: "#",
-        },
-        {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
-          url: "#",
-        },
-      ],
+    user: {
+        name: 'shadcn',
+        email: 'm@example.com',
+        avatar: '/avatars/shadcn.jpg',
     },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
+    navMain: [
         {
-          title: "Genesis",
-          url: "#",
+            title: 'Dashboard',
+            url: '#',
+            icon: LayoutDashboard,
+            isActive: true,
+            items: [
+                {
+                    title: 'Atendimentos',
+                    url: '#',
+                },
+                {
+                    title: 'Planejamento',
+                    url: '#',
+                },
+                {
+                    title: 'Financeiro',
+                    url: '#',
+                },
+            ],
         },
         {
-          title: "Explorer",
-          url: "#",
+            title: 'Cadastro',
+            url: '#',
+            icon: UserPlus,
+            items: [
+                {
+                    title: 'Cadastrar Terapeuta',
+                    url: '#',
+                },
+                {
+                    title: 'Cadastrar Paciente',
+                    url: '#',
+                },
+            ],
         },
         {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
+            title: 'Consulta',
+            url: '#',
+            icon: UserRoundSearchIcon,
+            items: [
+                {
+                    title: 'Terapeuta',
+                    url: '#',
+                },
+                {
+                    title: 'Paciente',
+                    url: '#',
+                },
+            ],
         },
         {
-          title: "Tutorials",
-          url: "#",
+            title: 'Configuração',
+            url: '#',
+            icon: Settings2,
+            items: [
+                {
+                    title: 'Geral',
+                    url: '#',
+                },
+                {
+                    title: 'Team',
+                    url: '#',
+                },
+                {
+                    title: 'Faturamento',
+                    url: '#',
+                },
+                {
+                    title: 'Limites',
+                    url: '#',
+                },
+            ],
+        },
+    ],
+    navSecondary: [
+        {
+            title: 'Support',
+            url: '#',
+            icon: LifeBuoy,
         },
         {
-          title: "Changelog",
-          url: "#",
+            title: 'Feedback',
+            url: '#',
+            icon: Send,
         },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
+    ],
+    projects: [
         {
-          title: "General",
-          url: "#",
+            name: 'Engineering',
+            url: '#',
+            icon: Frame,
         },
         {
-          title: "Team",
-          url: "#",
+            name: 'Vendas',
+            url: '#',
+            icon: PieChart,
         },
         {
-          title: "Billing",
-          url: "#",
+            name: 'Viagens',
+            url: '#',
+            icon: Map,
         },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Support",
-      url: "#",
-      icon: LifeBuoy,
-    },
-    {
-      title: "Feedback",
-      url: "#",
-      icon: Send,
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
-}
+    ],
+};
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const { user } = useAuth();
-  const displayName = user?.name || "teste";
-  const displayEmail = user?.email || "teste@teste.com"
-  const displayAvatar = "/avatars/shadcn.jpg"
+    const { user } = useAuth();
+    const displayName = user?.name || 'teste';
+    const displayEmail = user?.email || 'teste@teste.com';
+    const displayAvatar = '/avatars/shadcn.jpg';
 
-  return (
-    <Sidebar variant="inset" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              <a href="#">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Command className="size-4" />
-                </div>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">Acme Inc</span>
-                  <span className="truncate text-xs">Enterprise</span>
-                </div>
-              </a>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={{ name: displayName, email: displayEmail, avatar: displayAvatar }} />
-      </SidebarFooter>
-    </Sidebar>
-  )
+    return (
+        <Sidebar variant="inset" {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <a href="#">
+                                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                                    <Command className="size-4" />
+                                </div>
+                                <div className="grid flex-1 text-left text-sm leading-tight">
+                                    <span className="truncate font-medium">Instituto Índigo</span>
+                                    <span className="truncate text-xs">Administrador</span>
+                                </div>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain} />
+                <NavProjects projects={data.projects} />
+                <NavSecondary items={data.navSecondary} className="mt-auto" />
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser user={{ name: displayName, email: displayEmail, avatar: displayAvatar }} />
+            </SidebarFooter>
+        </Sidebar>
+    );
 }
