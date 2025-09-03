2f3ec8d4ca25fdaea3d21942ff18ebc9604c82e1
Natan Gomes <natantatan2000@gmail.com>
2025-09-03 14:32:38 -0300

    feat: estrutura inicial do sidebar

--- frontend/src/features/shell/layouts/AppLayout.tsx
+++ frontend/src/features/shell/layouts/AppLayout.tsx
@@ -1,12 +1,20 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { AppSidebar } from '@/components/app-sidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
 export default function AppLayout() {
     return (
-        <div className="min-h-screen bg-gray-100">
-            <h1>AppLayout funcionando!</h1>
-            <div>
-                <Outlet />
-            </div>
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset className="flex min-h-screen flex-col">
+                <Header />
+                <div className="flex-1 p-4">
+                    <Outlet />
+                </div>
+                <Footer />
+            </SidebarInset>
+        </SidebarProvider>
     );
 }
