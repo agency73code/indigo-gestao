257ecc1b21e80aaa918246ebd601d4c33f2bc691
Natan Gomes <natantatan2000@gmail.com>
2025-09-07 00:35:58 -0300

    header estrturado

--- frontend/src/features/shell/components/Header.tsx
+++ frontend/src/features/shell/components/Header.tsx
@@ -1,18 +1,34 @@
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import ThemeToggle from './ThemeToggle';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { BreadcrumbPage } from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
 
 export default function Header() {
     return (
-        <header className="bg-background border-b border-border min-h-screen m-3 rounded-[5px] flex justify-between shadow-sm">
-            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-                <div className="flex gap-2">
-                    <SidebarTrigger />
-                </div>
-
-                <div className="flex gap-2">
-                    <ThemeToggle />
-                </div>
-            </div>
+        <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb className='flex justify-between w-full'>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+            <ThemeToggle />
+          </Breadcrumb>
         </header>
     );
 }
